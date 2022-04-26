c097b3c410fbb03bc8a29680c1962c893a05087b
Ahmed <suwediahmed@gmail.com>
2022-04-26 12:38:41 +0800

    Update index.js
    changed name of app

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Spendee
+          Welcome to Ewallet
         </h1>
         </main>
     </div>
